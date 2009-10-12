531bc2e02435b332a72e85b639752497904d4792
Andy Chu <andy@chubot.org>
2009-10-11 23:34:57 -0700

    Got test AsFunction passing.

--- javascript/json-template-test.js
+++ javascript/json-template-test.js
@@ -4,9 +4,31 @@ jsUnity.log = print;  // From cscript-shell
 
 var results = jsUnity.run({
   // for fixing bug
-  testNoOptions: function () {
-    var t = jsontemplate.Template('Hello {name}');
+  testMoreFormattersAsFunction: function () {
+
+    formatters = function(user_str) {
+      if (user_str == 'lower') {
+        return function (s) { return s.toLowerCase(); };
+      } else if (user_str == 'upper') {
+        return function (s) { return s.toUpperCase(); };
+      } else {
+        return null;
+      }
+    };
+
+    var t = jsontemplate.Template(
+        'Hello {name|lower} {name|upper}',
+        {more_formatters: formatters});
+
     var actual = t.expand({'name': 'World'});
-    jsUnity.assertions.assertEqual(actual, 'Hello World');
+    jsUnity.assertions.assertEqual(actual, 'Hello world WORLD');
+  },
+
+  testMoreFormattersAsObject: function () {
+    // TODO: Make this work
+    formatters = {
+        'lower': function (s) { return s.toLowerCase(); },
+        'upper': function (s) { return s.toUpperCase(); }
+        };
   }
-})
+});
