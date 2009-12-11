953dbf472019d63ea3baeef67313570b00aa485f
Andy Chu <andy@chubot.org>
2009-12-10 22:01:05 -0800

    Parsing JSON correctly

--- lua/verifier.lua
+++ lua/verifier.lua
@@ -3,16 +3,12 @@
 json = require "Json"
 jsontemplate = require "json-template"
 
-
 template_str = arg[1]
 data_json = arg[2]
 options_json = arg[3]
--- print (template_str)
--- print (data_json)
--- print (options_json)
 
--- data = json:Decode(data_json)
--- options = json:Decode(options_json)
+data = json.Decode(data_json)
+options = json.Decode(options_json)
 
-local t = jsontemplate.Template(template_str)
-print(t.expand(data_json))
+local t = jsontemplate.Template(template_str, options)
+print(t.expand(data))
