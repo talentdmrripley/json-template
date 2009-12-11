4c5217210d5f1f3087d6b37a5f0d5d5f81237ded
Andy Chu <andy@chubot.org>
2009-12-10 20:37:25 -0800

    json-template.lua importing

--- lua/verifier.lua
+++ lua/verifier.lua
@@ -1,7 +1,11 @@
 -- Command line program called by verifier.py
 
 json = require "Json"
+jsontemplate = require "json-template"
 
-o = json.Decode('{"a": 3}');
+o = json.Decode('{"a": 3}')
 print(o)
 print(o.a)
+
+o = jsontemplate.Template('{foo}')
+print(o)
