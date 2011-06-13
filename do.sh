56badb847dd89b9b9b34eec90ab14f63bbd49710
Andy Chu <andy@chubot.org>
2011-06-12 17:17:27 -0700

    Various scripts for running subsets of the tests

--- do.sh
+++ do.sh
@@ -5,10 +5,34 @@
 action=$1
 shift
 
+PAN_DIR=~/svn/pan/trunk
+
 case $action in
 
   pygrep)
     find . -name \*.py | xargs grep "$@"
+    ;;
+
+  py-tests)
+    # Run Python unit tests
+    export PYTHONPATH=$PAN_DIR
+    set -o errexit
+    ./jsontemplate_test.py
+    python/jsontemplate/jsontemplate_unittest.py
+    ;;
+
+  js-tests)
+    set -o errexit
+    ./js_tests.sh
+    export PYTHONPATH=$PAN_DIR
+    ./jsontemplate_test.py --javascript
+    ;;
+
+  all-tests)
+    # Run all unit tests
+    export PYTHONPATH=$PAN_DIR
+    set -o errexit
+    ./jsontemplate_test.py --all
     ;;
 
   *)
