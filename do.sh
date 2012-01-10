f8c8dd39f02b9919e10518f00cde7731f4392bf8
Andy Chu <andy@chubot.org>
2012-01-09 23:00:15 -0800

    convert to new style

--- do.sh
+++ do.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
 #
-# Misc utilities
-
-action=$1
-shift
+# Scripts for JSON Template
+#
+# Usage:
+#   ./do.sh <action> <args>
 
 TASTE_DIR=~/hg/taste
 
-case $action in
+pygrep() {
+  find . -name \*.py | xargs grep "$@"
+}
 
-  pygrep)
-    find . -name \*.py | xargs grep "$@"
-    ;;
+py-tests() {
+  # Run Python unit tests
+  export PYTHONPATH=$TASTE_DIR
+  set -o errexit
+  ./jsontemplate_test.py "$@"
+  python/jsontemplate/jsontemplate_unittest.py
+  python/jsontemplate/formatters_test.py
+}
 
-  py-tests)
-    # Run Python unit tests
-    export PYTHONPATH=$TASTE_DIR
-    set -o errexit
-    ./jsontemplate_test.py "$@"
-    python/jsontemplate/jsontemplate_unittest.py
-    python/jsontemplate/formatters_test.py
-    ;;
+js-tests() {
+  set -o errexit
+  ./js_tests.sh
+  export PYTHONPATH=$TASTE_DIR
+  ./jsontemplate_test.py --javascript
+}
 
-  js-tests)
-    set -o errexit
-    ./js_tests.sh
-    export PYTHONPATH=$TASTE_DIR
-    ./jsontemplate_test.py --javascript
-    ;;
+all-tests() {
+  # Run all unit tests
+  export PYTHONPATH=$TASTE_DIR
+  set -o errexit
+  ./jsontemplate_test.py --all
+}
 
-  all-tests)
-    # Run all unit tests
-    export PYTHONPATH=$TASTE_DIR
-    set -o errexit
-    ./jsontemplate_test.py --all
-    ;;
-
-  *)
-    echo "Invalid action '$action'"
-    exit 1
-    ;;
-esac
+"$@"
